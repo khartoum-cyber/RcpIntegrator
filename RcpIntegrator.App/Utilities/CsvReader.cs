1ddce283a48223e96ce1da70b2e41d06d4e40fa6
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-12-22 09:37:35 +0100

    csvReader utility

---
+++ RcpIntegrator.App/Utilities/CsvReader.cs
@@ -0,0 +1,22 @@
+﻿namespace RcpIntegrator.App.Utilities
+{
+    public static class CsvReader
+    {
+        public static IEnumerable<string[]> ReadSemicolonSeparated(Stream stream)
+        {
+            using var reader = new StreamReader(stream);
+            string? line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                //CsvReader
+                var parts = line.Split(';');
+
+                yield return parts;
+            }
+        }
+    }
+}
