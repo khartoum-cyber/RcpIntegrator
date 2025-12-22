fbc419cfdde3d82c1d8e932219ce6d11f0060482
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-12-22 09:33:25 +0100

    workDay model

---
+++ RcpIntegrator.App/Model/WorkDay.cs
@@ -0,0 +1,20 @@
+﻿namespace RcpIntegrator.App.Model
+{
+    internal class WorkDay
+    {
+        public string Company { get; }
+        public string EmployeeCode { get; }
+        public DateTime Date { get; }
+        public TimeSpan EntryTime { get; }
+        public TimeSpan ExitTime { get; }
+
+        public WorkDay(string company, string employeeCode, DateTime date, TimeSpan entry, TimeSpan exit)
+        {
+            Company = company;
+            EmployeeCode = employeeCode;
+            Date = date;
+            EntryTime = entry;
+            ExitTime = exit;
+        }
+    }
+}
