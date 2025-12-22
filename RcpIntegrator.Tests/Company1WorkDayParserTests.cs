ce17cd84ae3e04fa9a152cfec632f23d59b8d85f
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-12-22 10:15:23 +0100

    company1 test

---
+++ RcpIntegrator.Tests/Company1WorkDayParserTests.cs
@@ -0,0 +1,27 @@
+﻿using RcpIntegrator.App.Services.Parsers;
+
+namespace RcpIntegrator.Tests
+{
+    public class Company1WorkDayParserTests
+    {
+        [Test]
+        public void Parse_ValidRow_YieldsWorkDay()
+        {
+            var csv = "EMP001; 2025-12-19; 08:00; 16:00; note\n";
+            using var ms = CsvHelpers.ToStream(csv);
+
+            var parser = new Company1WorkDayParser();
+
+            var result = parser.Parse(ms).ToList();
+
+            Assert.That(result, Has.Count.EqualTo(1));
+            var wd = result[0];
+
+            Assert.That(wd.Company, Is.EqualTo("Company1"));
+            Assert.That(wd.EmployeeCode, Is.EqualTo("EMP001"));
+            Assert.That(wd.Date.Date, Is.EqualTo(new DateTime(2025, 12, 19)));
+            Assert.That(wd.EntryTime, Is.EqualTo(TimeSpan.FromHours(8)));
+            Assert.That(wd.ExitTime, Is.EqualTo(TimeSpan.FromHours(16)));
+        }
+    }
+}
---
+++ RcpIntegrator.Tests/CsvHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace RcpIntegrator.Tests
+{
+    static class CsvHelpers
+    {
+        public static MemoryStream ToStream(string content)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(content));
+        }
+    }
+}
