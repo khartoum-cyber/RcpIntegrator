18e17de50e24580caecdf36abe89290ec36be8ef
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-12-22 12:25:24 +0100

    company1 2nd test

--- RcpIntegrator.Tests/Company1WorkDayParserTests.cs
+++ RcpIntegrator.Tests/Company1WorkDayParserTests.cs
@@ -1,4 +1,6 @@
-﻿using RcpIntegrator.App.Services.Parsers;
+﻿using RcpIntegrator.App.Model;
+using RcpIntegrator.App.Services.Parsers;
+using NUnit.Framework;
 
 namespace RcpIntegrator.Tests
 {
@@ -23,5 +25,38 @@ namespace RcpIntegrator.Tests
             Assert.That(wd.EntryTime, Is.EqualTo(TimeSpan.FromHours(8)));
             Assert.That(wd.ExitTime, Is.EqualTo(TimeSpan.FromHours(16)));
         }
+
+        [Test]
+        public void Parse_MixedValidInvalid_OnlyValidReturned()
+        {
+
+            var csv = string.Join("\n", new[]
+            {
+                "EMP001; 2025-12-19; 08:00; 16:00; ok",
+                "EMP001; not-a-date; 08:00; 16:00; bad",
+                "EMP002; 2025-12-20; 09:00; 17:00; ok",
+                "EMP003; 2025-12-21; 09:00; not-a-time; bad",
+            }) + "\n";
+
+            using var ms = CsvHelpers.ToStream(csv);
+            var parser = new Company1WorkDayParser();
+
+            var result = parser.Parse(ms).ToList();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Count, Is.EqualTo(2));
+
+                // All returned items must be from Company1
+                Assert.That(result, Has.All.Matches<WorkDay>(wd => wd.Company == "Company1"));
+
+                // Collection should contain EMP001 and EMP002
+                Assert.That(result, Has.Some.Matches<WorkDay>(wd => wd.EmployeeCode == "EMP001"));
+                Assert.That(result, Has.Some.Matches<WorkDay>(wd => wd.EmployeeCode == "EMP002"));
+            });
+        }
+
     }
+
+
 }
