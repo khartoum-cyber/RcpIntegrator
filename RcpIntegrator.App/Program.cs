6cd4c9bae023dddfadd8a7d91ff7c5c8049a733b
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-12-22 09:54:47 +0100

    entry point logic

--- RcpIntegrator.App/Program.cs
+++ RcpIntegrator.App/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace RcpIntegrator.App
+﻿using RcpIntegrator.App.Services;
+using RcpIntegrator.App.Services.Parsers;
+
+namespace RcpIntegrator.App
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            using var fs1 = File.OpenRead("rcp1.csv");
+            //using var fs2 = File.OpenRead("rcp2.csv");
+
+            var service = new WorkDayService();
+            var workDays = service.LoadUnique(
+                fs1, new Company1WorkDayParser());
+
+            // Print
+            foreach (var wd in workDays.OrderBy(w => w.Company).ThenBy(w => w.EmployeeCode).ThenBy(w => w.Date))
+            {
+                Console.WriteLine(wd.EmployeeCode);
+            }
         }
     }
 }
