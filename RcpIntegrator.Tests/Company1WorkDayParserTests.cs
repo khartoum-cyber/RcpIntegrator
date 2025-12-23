470c20468bb74cab05dc6caacc3bc89bda36c083
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-12-23 11:35:30 +0100

    test fixes

--- RcpIntegrator.Tests/Company1WorkDayParserTests.cs
+++ RcpIntegrator.Tests/Company1WorkDayParserTests.cs
@@ -1,6 +1,5 @@
 ﻿using RcpIntegrator.App.Model;
 using RcpIntegrator.App.Services.Parsers;
-using NUnit.Framework;
 
 namespace RcpIntegrator.Tests
 {
@@ -9,7 +8,7 @@ namespace RcpIntegrator.Tests
         [Test]
         public void Parse_ValidRow_YieldsWorkDay()
         {
-            var csv = "EMP001; 2025-12-19; 08:00; 16:00; note\n";
+            var csv = "3425; 2025-12-19; 08:00; 16:00; note\n";
             using var ms = CsvHelpers.ToStream(csv);
 
             var parser = new Company1WorkDayParser();
@@ -20,7 +19,7 @@ namespace RcpIntegrator.Tests
             var wd = result[0];
 
             Assert.That(wd.Company, Is.EqualTo("Company1"));
-            Assert.That(wd.EmployeeCode, Is.EqualTo("EMP001"));
+            Assert.That(wd.EmployeeCode, Is.EqualTo(3425));
             Assert.That(wd.Date.Date, Is.EqualTo(new DateTime(2025, 12, 19)));
             Assert.That(wd.EntryTime, Is.EqualTo(TimeSpan.FromHours(8)));
             Assert.That(wd.ExitTime, Is.EqualTo(TimeSpan.FromHours(16)));
@@ -32,10 +31,11 @@ namespace RcpIntegrator.Tests
 
             var csv = string.Join("\n", new[]
             {
-                "EMP001; 2025-12-19; 08:00; 16:00; ok",
-                "EMP001; not-a-date; 08:00; 16:00; bad",
-                "EMP002; 2025-12-20; 09:00; 17:00; ok",
-                "EMP003; 2025-12-21; 09:00; not-a-time; bad",
+                "3425; 2025-12-19; 08:00; 16:00; ok",
+                "3425; not-a-date; 08:00; 16:00; bad",
+                "344;  2025-12-20; 09:00; 17:00; ok",
+                "9999; 2025-12-21; 09:00; not-a-time; bad",
+
             }) + "\n";
 
             using var ms = CsvHelpers.ToStream(csv);
@@ -50,9 +50,9 @@ namespace RcpIntegrator.Tests
                 // All returned items must be from Company1
                 Assert.That(result, Has.All.Matches<WorkDay>(wd => wd.Company == "Company1"));
 
-                // Collection should contain EMP001 and EMP002
-                Assert.That(result, Has.Some.Matches<WorkDay>(wd => wd.EmployeeCode == "EMP001"));
-                Assert.That(result, Has.Some.Matches<WorkDay>(wd => wd.EmployeeCode == "EMP002"));
+                // Collection should contain codes 3425 and 344
+                Assert.That(result, Has.Some.Matches<WorkDay>(wd => wd.EmployeeCode == 3425));
+                Assert.That(result, Has.Some.Matches<WorkDay>(wd => wd.EmployeeCode == 344));
             });
         }
 
